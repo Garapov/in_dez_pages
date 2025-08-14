e8c39abaf99a3a174b781f6a5d2a87c2727c59f0
Руслан Гарапов <ruslangarapov@MacBookRuslan.local>
2025-08-14 18:24:08 +0500

    Slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log("Vite запущен!");
+import Glide from "@glidejs/glide";
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector(".new_reviews.glide")) {
+    window.reviews_slider = new Glide(".new_reviews.glide", {
+      perView: 4,
+      bound: true,
+      gap: 20,
+    }).mount();
+  }
+});
